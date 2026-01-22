e5f6b1186e62b952580dcd0e434802012926a503
NikhilPachouri <nikhilpachouri555@gmail.com>
2026-01-22 19:56:06 +0530

    some changes

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -4,13 +4,20 @@
 #include "color.h"
 
 
+bool hit_sphere(const point3& centre,double radius,const ray& r){
+vec3 oc = centre - r.origin();
+auto a = dot(r.direction(),r.direction());
+auto b = -2.0 * dot(r.direction(),oc);
+auto c = dot(c,c) - radius.radius;
 
-
+auto discriminant = b*b - 4*a*c;
+return (discriminant >= 0);
+}
 
 color ray_color (const ray& r){
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    unit_direction = unit_vector(r.ray_direction());
+    auto a = 0.5(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0). + a*color(0.5, 0.7, 1.0);
 
 }
 
@@ -20,7 +27,6 @@ int main() {
     int image_width = 400;
     int image_height = int(image_width/aspect_ratio);
     image_height = (image_height<1) ? 1: image_height;
-
 
 
     auto focal_length = 1.0;
@@ -39,16 +45,18 @@ int main() {
 
 std::cout << "P3\n" 
           << image_width << ' ' << image_height 
-          << "\n255\n";
+          << "\n255\n";s
 
 for (int j =0;j < image_height;j++){
     std::clog <<"\rScanlines remaining" <<(image_height - j) <<' '<<std::flush;
 
     for(int i = 0; i<image_width; i++){
-        auto pixel_centre = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
-        auto ray_direction = pixel_centre - camera_center;
+    
+
+        pixel_centre = pixel00_loc + (i*pixel_delta_u) + (j*pixel_delta_v);
+        ray_direction = pixel_centre - camera_center;
         ray r(camera_center,ray_direction);
-        color pixel_color = ray_color(r);
+        pixel_color = ray_color(r);
         write_color(std::cout,pixel_color);
 
 
